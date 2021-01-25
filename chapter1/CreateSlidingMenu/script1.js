d077fbabc7f25157f0cb1ba753d3537c0c22992b
Halfinity <ofirhalfin13@gmail.com>
2021-01-25 17:59:07 +0200

    CreateSlidingMenu :ok_hand:

---
+++ chapter1/CreateSlidingMenu/script1.js
@@ -0,0 +1,18 @@
+let roundButton = document.querySelector("#roundButton");
+roundButton.addEventListener("click", showMenu, false);
+ 
+let flyoutMenu = document.querySelector("#flyoutMenu");
+flyoutMenu.addEventListener("click", hideMenu, false);
+ 
+function showMenu(e) {
+    flyoutMenu.classList.add("show");
+ 
+    document.body.style.overflow = "hidden";
+}
+ 
+function hideMenu(e) {
+    flyoutMenu.classList.remove("show");
+    e.stopPropagation();
+ 
+    document.body.style.overflow = "auto";
+}  
