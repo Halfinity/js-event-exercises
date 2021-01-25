a987acdd03ba5137d7b997c38e8ae7b48ef7fed9
Halfinity <ofirhalfin13@gmail.com>
2021-01-25 20:59:16 +0200

    Whoops, Swap Names

--- chapter3/ToolTip/script3.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('.tree-structure').addEventListener('click', (e) => {
-    const el = e.target;
-    const sibling = el.nextSibling.nextSibling;
-  
-    if (el && el.className == 'toggle' && sibling) {
-      sibling.classList.toggle('show');
-    }
-  });
